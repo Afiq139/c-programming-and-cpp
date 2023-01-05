763a3dc7e0d653ecb9a77cc14befde00d781b349
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-06 03:35:17 +0800

    update

--- C_Programming_tutorial2/main.c
+++ C_Programming_tutorial2/main.c
@@ -9,6 +9,23 @@ int main()
     printf("----------------------------------- \n");
     printf(" \n");
 
+    double num1;
+    double num2;
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    printf("Answer: %0.2f", num1 + num2);
+
+
+    printf(" \n");
+    printf(" \n");
+    printf("----------------------------------- \n");
+    printf("Building a Mad Libs Game - 1.17.42\n");
+    printf("----------------------------------- \n");
+    printf(" \n");
+
 
 
     return 0;
