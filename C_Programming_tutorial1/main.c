61ae7052d13ab46db049ff1ecac5cdbb09f9037c
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-05 18:32:14 +0800

    update

---
+++ C_Programming_tutorial1/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("----------------------------------- \n");
+    printf("Introduction\n");
+    printf("----------------------------------- \n");
+    printf(" \n");
+    printf("Hello world!\n");
+    printf(" \n");
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
+    printf(" \n");
+    printf(" \n");
+    printf("----------------------------------- \n");
+    printf("Variables - 21.21\n");
+    printf("----------------------------------- \n");
+    printf(" \n");
+
+    char characterName[] = "Shafiq";
+    int characterAge = 32;
+    printf("There once was a man named %s\n", characterName);
+    printf("He was %d years old.\n",characterAge);
+
+    characterAge = 21;
+    printf("He was really liked the name %s\n",characterName);
+    printf("but did not like being %d.\n", characterAge);
+
+    printf(" \n");
+    printf(" \n");
+    printf("----------------------------------- \n");
+    printf("Data Types - 32.26\n");
+    printf("----------------------------------- \n");
+    printf(" \n");
+
+    int age = 40;
+    printf("Age = %d \n", age);
+
+    double gpa = 3.6;
+    printf("Marks = %0.1f \n",gpa);
+    printf("Marks = %0.2f \n",gpa);
+    printf("Marks = %0.3f \n",gpa);
+
+    char grade = 'A';
+    printf("Grade = %c \n", grade);
+
+    char phrase[]  = "Shafiq Engineering";
+    printf("Phrase = %s \n", phrase);
+
+
+    printf(" \n");
+    printf(" \n");
+    printf("----------------------------------- \n");
+    printf("Printf - 38.32\n");
+    printf("----------------------------------- \n");
+    printf(" \n");
+    printf("Hello\nWorld");
+    printf("Hello\"World");
+
+    printf(" \n");
+    printf(" \n");
+
+    return 0;
+}
