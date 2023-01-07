4655af3f0e74500ea6b28fc49fb9ae79ceb053f1
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-07 23:09:53 +0800

    update

--- C_Programming_tutorial4/main.c
+++ C_Programming_tutorial4/main.c
@@ -8,9 +8,59 @@ int main()
     printf("----------------------------------- \n");
     printf(" \n");
 
+    int age = 30;
+    double gpa = 3.4;
+    char grade = 'A';
+
+    printf("Age: %p\nGpa: %p\nGrade: %p", &age, &gpa, &grade);
+    /* accessing physical memory address */
+
     printf(" \n");
+    printf(" \n");
+    printf("----------------------------------- \n");
+    printf("Pointers - 3:17:21\n");
+    printf("----------------------------------- \n");
+    printf(" \n");
+
+    int ages = 30;
+    /* int * pAges = &ages; */
+    /* double gpas = 3.4; */
+    /* double * pGpas = &gpas; */
+    /* char grades = 'A'; */
+    /* char * pGrades = &grades; */
+
+    printf("age's memory address: %p\n", &ages);
+    /* pointer is just a data type of memory address. can make pointer variables */
+
+
+    printf(" \n");
+    printf(" \n");
+    printf("----------------------------------- \n");
+    printf("Dereferencing Pointers - 3:27:42\n");
+    printf("----------------------------------- \n");
+    printf(" \n");
+
+    int agess = 30;
+    int *pAgess = &agess;
+
+    printf("%d \n", *pAgess);
+    printf("%d \n", *&agess);
+    printf("%p \n", &agess);
+    printf("%d \n", *&*&agess);
+
+    /*  *-> deference address, & -> reference address */
+
+
+    printf(" \n");
+    printf(" \n");
+    printf("----------------------------------- \n");
+    printf("Writing Files - 3:32:38\n");
+    printf("----------------------------------- \n");
     printf(" \n");
 
 
+
+    printf(" \n");
+    printf(" \n");
     return 0;
 }
