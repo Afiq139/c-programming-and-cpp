7f904562069f734597479c87022d372f9847e009
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-10 06:28:54 +0800

    update

--- C++/Tutorial1/main.cpp
+++ C++/Tutorial1/main.cpp
@@ -160,9 +160,25 @@ int main()
     cout << "----------------------------" << endl;
     cout << "" << endl;
 
+    double num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "" << endl;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << num1 + num2;
+
 
     cout << "" << endl;
     cout << "" << endl;
+    cout << "----------------------------" << endl;
+    cout << "Building a Mad Libs Game --- 1:09:27" << endl;
+    cout << "----------------------------" << endl;
+    cout << "" << endl;
+
 
     return 0;
 }
