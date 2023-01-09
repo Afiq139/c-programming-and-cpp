0d97b046fae042997ff0668db79fb76073ff4904
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-09 20:54:45 +0800

    Update

---
+++ C++/Tutorial1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----------------------------" << endl;
+    cout << "Drawing a Shape ---12.30" << endl;
+    cout << "----------------------------" << endl;
+    cout << "" << endl;
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+
+
+    cout << "" << endl;
+    cout << "" << endl;
+    /* cout - console out */
+    /* endl - endline */
+
+    cout << "----------------------------" << endl;
+    cout << "Variables ---19.54" << endl;
+    cout << "----------------------------" << endl;
+    cout << "" << endl;
+
+    cout << "there once was a man named George" << endl;
+    cout << "He was 70 years old" << endl;
+    cout << "He like the name George" << endl;
+    cout << "But did not like being 70" << endl;
+
+
+    cout << "" << endl;
+    cout << "" << endl;
+    return 0;
+}
