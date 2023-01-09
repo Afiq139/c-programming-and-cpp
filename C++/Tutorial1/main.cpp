f23ad069c042fe0beec7517e6d22ee2e468b1e1c
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-09 23:35:05 +0800

    update

--- C++/Tutorial1/main.cpp
+++ C++/Tutorial1/main.cpp
@@ -45,9 +45,45 @@ int main()
     cout << "Data Types --- 31.43" << endl;
     cout << "----------------------------" << endl;
     cout << "" << endl;
+        /* data types - types of information 32.47 */
+    char grade = 'A'; /*1 character */
+    string phrase = "Shafiq Engineering"; /* many Characters */
+    float gpa1 = 3.3;   /*few decimals*/
+    double  gpa2 = 3.3;  /*many decimals*/
+    bool isMale = true; /*true or false */
+
+    cout << grade << endl;
+    cout << phrase << endl;
+    cout << gpa1 << endl;
+    cout << gpa2 << endl;
+    cout << isMale << endl;
+
+    cout << "" << endl;
+    cout << "" << endl;
+
+    cout << "----------------------------" << endl;
+    cout << "Working with Strings --- 39.15" << endl;
+    cout << "----------------------------" << endl;
+    cout << "" << endl;
+
+    cout << "Shafiq Engineering\n";
+    cout << "Hello" << endl;
+
+    string phrase1 = "Shafiq Engineering";
+    cout << phrase1 << endl;;
+    cout << phrase1.length() << endl; /* how many characters in that variable */
+    cout << phrase1[0] << endl; /* accessing individual characters */
+    cout << phrase1[4] << endl;
+
+    phrase1[3] = 'B';
+    cout << phrase1[3]<< endl;
+    cout << phrase1 << endl;;
+
+    cout << phrase1.find("Engineering", 0); /* finding index position from 0 */
 
 
     cout << "" << endl;
     cout << "" << endl;
+
     return 0;
 }
