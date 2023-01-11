05585b483984c924a8877f9597e0be59ad5969d8
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-12 01:13:57 +0800

    Update

--- C++/Tutorial2/main.cpp
+++ C++/Tutorial2/main.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+/*void sayHi(string name, int age){
+    cout << "Hello " << name << ".\nYou are "<< age << " years old."<< endl;
+     }
+*/ /* Function */
+
+
+void sayHi(string name, int age); /*function's stop/signature   */
+
 int main()
 {
 
@@ -29,6 +37,58 @@ int main()
 
     cout << "" << endl;
     cout << "" << endl;
+    cout << "----------------------------" << endl;
+    cout << "Arrays --- 1:13:44" << endl;
+    cout << "----------------------------" << endl;
+    cout << "" << endl;
+
+    /*int luckyName[] = {4, 8, 15, 16, 23, 42};
+    luckyName[0] = 19;
+    cout << luckyName[0]; */
+
+    /*int luckyName[20] = {4, 8, 15, 16, 23, 42};
+    luckyName[10] = 100;
+    cout << luckyName[10]; */
+
+    int luckyName[20];
+    luckyName[10] = 100;
+    cout << luckyName[10];
+
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "----------------------------" << endl;
+    cout << "Functions --- 1.20.03 " << endl;
+    cout << "----------------------------" << endl;
+    cout << "" << endl;
+
+    cout << "Top" << endl;
+    cout << "" << endl;
+    sayHi("Shafiq", 32); /* calling function */
+    cout << "" << endl;
+    sayHi("Elia", 13);
+    cout << "" << endl;
+    sayHi("Nani", 24);
+    cout << "" << endl;
+    cout << "Bottom" << endl;
+
+
+
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "----------------------------" << endl;
+    cout << "Return Statement --- 1.29.47 " << endl;
+    cout << "----------------------------" << endl;
+    cout << "" << endl;
+
+
+
+
+    cout << "" << endl;
+    cout << "" << endl;
 
     return 0;
+}
+
+void sayHi(string name, int age){
+    cout << "Hello " << name << ".\nYou are "<< age << " years old."<< endl;
 }
