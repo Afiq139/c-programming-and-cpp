aa3a6e874584c595b7af6c13dea554d322ef5bbc
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-14 00:58:25 +0800

    Update

--- C++/Tutorial3/main.cpp
+++ C++/Tutorial3/main.cpp
@@ -93,6 +93,22 @@ int main()
     cout << "-------------------------------------" << endl;
     cout << "" << endl;
 
+    int numberGrid[3][2] = {
+                            {1, 2},
+                            {3, 4},
+                            {5, 6},
+    };
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 2; j++){
+            cout << numberGrid[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "" << endl;
+    cout << numberGrid[0][1];
+
 
     cout << "" << endl;
     cout << "" << endl;
