4096f59548945bd007909af930f1a87830f12a6d
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-17 22:05:53 +0800

    2.21

--- C++/C++ Projects/1_Online_banking_System/main.c
+++ C++/C++ Projects/1_Online_banking_System/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+//Online Banking System
+
+//define user
+struct user{
+    char phone[50];
+    char ac[50];  //account
+    char password[50];
+    float balance;
+
+};
 int main()
 {
-    printf("Hello world!\n");
+    struct user usr;
+
+    printf("\nWhat do you want to do?");
+    printf("\n\n1. Register an Account");
+    printf("\n2. Login to Account");
+
+
+
     return 0;
 }
