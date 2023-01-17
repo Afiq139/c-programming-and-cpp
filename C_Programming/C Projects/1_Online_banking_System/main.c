a3d2fb22bd1a381765ddbcf8a32ab27b74fdc801
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-17 22:18:07 +0800

    update

--- C_Programming/C Projects/1_Online_banking_System/main.c
+++ C_Programming/C Projects/1_Online_banking_System/main.c
@@ -15,10 +15,25 @@ struct user{
 int main()
 {
     struct user usr;
+    int opt;
 
     printf("\nWhat do you want to do?");
     printf("\n\n1. Register an Account");
-    printf("\n2. Login to Account");
+    printf("\n2. Login to an Account");
+
+    printf("\n\nYour choice:\t");
+    scanf("%d", &opt);
+
+    if(opt == 1){
+        system("cls"); //clear in linux or mac
+        printf("Enter your account no:\t");
+        scanf("%s", usr.ac);
+        printf("\nEnter your phone no:\t");
+        scanf("%s", usr.phone);
+        printf("\nEnter your new password:\t");
+        scanf("%s", usr.password);
+        usr.balance = 0;
+    }
 
 
 
