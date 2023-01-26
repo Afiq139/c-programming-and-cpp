a653cfc223dc2f4c38c18ca569f0dbe95df742c8
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-26 11:11:28 +0800

    update

---
+++ C++/C++ Projects/2. ATM Application/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void showMenu(){
+    cout << "**********Menu**********" << endl;
+    cout << "1. Check Balance " << endl;
+    cout << "2. Deposit " << endl;
+    cout << "3. Withdraw " << endl;
+    cout << "************************" << endl;
+}
+
+int main()
+{
+    //check balance, deposit, withdraw, show menu
+
+    showMenu();
+
+    //return 0;
+    system("pause>0");
+}
